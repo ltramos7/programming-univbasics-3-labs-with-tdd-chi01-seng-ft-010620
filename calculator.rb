f81cc1736f5c5b3af0aa6a11ae273ad984591100
Linda Ramos <ltramos7@gmail.com>
2019-11-11 22:12:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,12 @@
 # Add your variables here
+<<<<<<< HEAD
 first_number = 2.1
 second_number = 6
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
+=======
+var = first_number(Integer)
+Integer
+>>>>>>> fb3cfee3324b468a32478187e74f4790cdc3deb5
